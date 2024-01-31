182f3be6490ed22483988585d85cbbab983741b6
zakzakst <e1433104@gmail.com>
2024-01-31 19:59:23 +0900

    UIの改善

--- src/app/inventory/layout.tsx
+++ src/app/inventory/layout.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import {
+  createTheme,
   AppBar,
   Box,
   Button,
@@ -20,99 +23,141 @@ import {
   Menu as MenuIcon,
 } from '@mui/icons-material'
 
+declare module '@mui/material/styles' {
+  // 指定を単純にするためにモバイルとPCの2つに限定する
+  interface BreakpointOverrides {
+    xs: false
+    sm: false
+    md: false
+    lg: false
+    xl: false
+    mobile: true
+    desktop: true
+  }
+}
+
+const defaultTheme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      desktop: 600,
+    },
+  },
+})
+
 export default function InventoryLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      {/* ▼▼▼ ヘッダー ▼▼▼ */}
-      <AppBar position='fixed'>
-        <Toolbar>
-          <IconButton>
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant='h6'
-            noWrap
-            component='div'
-            sx={{ flexGrow: 1 }}
-          >
-            在庫管理システム
-          </Typography>
-          <Button
-            variant='contained'
-            startIcon={<LogoutIcon />}
-          >
-            ログアウト
-          </Button>
-        </Toolbar>
-      </AppBar>
-      {/* ▲▲▲ ヘッダー ▲▲▲ */}
+  // サイドバーの開閉を管理する
+  const [open, setOpen] = useState(false)
+  const toggleDrawer = (open: boolean) => {
+    setOpen(open)
+  }
 
-      {/* ▼▼▼ サイドバー ▼▼▼ */}
-      <Drawer anchor='left'>
-        <Box sx={{ width: 240 }}>
-          <Toolbar />
-          <Divider />
-          <List>
-            <ListItem
-              component='a'
-              href='/inventory/products'
-              disablePadding
-            >
-              <ListItemButton>
-                <ListItemText primary='商品一覧' />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem
-              component='a'
-              href='/inventory/import_sales'
-              disablePadding
-            >
-              <ListItemButton>
-                <ListItemText primary='売上一括登録' />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-          </List>
-        </Box>
-      </Drawer>
-      {/* ▲▲▲ サイドバー ▲▲▲ */}
+  // 各種画面への遷移を管理する
+  const router = useRouter()
+  const handleLogout = () => {
+    router.replace('/login')
+  }
 
-      {/* ▼▼▼ メインコンテンツ ▼▼▼ */}
-      <Box
-        component='main'
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          marginTop: '64px',
-          width: '100%',
-          background: 'white',
-        }}
-      >
-        {children}
-      </Box>
-      {/* ▲▲▲ メインコンテンツ ▲▲▲ */}
-
-      {/* ▼▼▼ フッター ▼▼▼ */}
-      <Box
-        component='footer'
-        sx={{
-          width: '100%',
-          position: 'fixed',
-          textAlign: 'center',
-          bottom: 0,
-          background: '#1976d2',
-        }}
-      >
-        <Typography variant='caption' color='white'>
-          @2023 full stack web development
-        </Typography>
-      </Box>
-      {/* ▲▲▲ フッター ▲▲▲ */}
+  // 開閉対象となるサイドバー本体
+  const list = () => (
+    <Box sx={{ width: 240 }}>
+      <Toolbar />
+      <Divider />
+      <List>
+        <ListItem
+          component='a'
+          href='/inventory/products'
+          disablePadding
+        >
+          <ListItemButton>
+            <ListItemText primary='商品一覧' />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem
+          component='a'
+          href='/inventory/import_sales'
+          disablePadding
+        >
+          <ListItemButton>
+            <ListItemText primary='売上一括登録' />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+      </List>
     </Box>
+  )
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: 'flex' }}>
+        {/* ▼▼▼ ヘッダー ▼▼▼ */}
+        <AppBar position='fixed'>
+          <Toolbar>
+            <IconButton onClick={() => toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant='h6'
+              noWrap
+              component='div'
+              sx={{ flexGrow: 1 }}
+            >
+              在庫管理システム
+            </Typography>
+            <Button
+              variant='contained'
+              startIcon={<LogoutIcon />}
+              onClick={() => handleLogout()}
+            >
+              ログアウト
+            </Button>
+          </Toolbar>
+        </AppBar>
+        {/* ▲▲▲ ヘッダー ▲▲▲ */}
+
+        {/* ▼▼▼ サイドバー ▼▼▼ */}
+        <Drawer open={open} onClose={() => toggleDrawer(false)} anchor='left'>
+          {list()}
+        </Drawer>
+        {/* ▲▲▲ サイドバー ▲▲▲ */}
+
+        {/* ▼▼▼ メインコンテンツ ▼▼▼ */}
+        <Box
+          component='main'
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            marginTop: '64px',
+            width: '100%',
+            background: 'white',
+          }}
+        >
+          {children}
+        </Box>
+        {/* ▲▲▲ メインコンテンツ ▲▲▲ */}
+
+        {/* ▼▼▼ フッター ▼▼▼ */}
+        <Box
+          component='footer'
+          sx={{
+            width: '100%',
+            position: 'fixed',
+            textAlign: 'center',
+            bottom: 0,
+            background: '#1976d2',
+          }}
+        >
+          <Typography variant='caption' color='white'>
+            @2023 full stack web development
+          </Typography>
+        </Box>
+        {/* ▲▲▲ フッター ▲▲▲ */}
+      </Box>
+    </ThemeProvider>
   )
 }
